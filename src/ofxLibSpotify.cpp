ee1f0bddbf38e336c8343e798d12ba1338a063f3
James Kong <jameskong@idthk.com>
2013-11-18 11:33:31 +0800

    init commit

---
+++ src/ofxLibSpotify.cpp
@@ -0,0 +1,17 @@
+//
+//  ofxLibSpotify.cpp
+//  
+//
+//  Created by James Kong on 18/11/13.
+//
+//
+
+#include "ofxLibSpotify.h"
+void ofxLibSpotify::setup(string user_name, string password)
+{
+    
+}
+void ofxLibSpotify::update()
+{
+    
+}
---
+++ src/ofxLibSpotify.h
@@ -0,0 +1,20 @@
+//
+//  ofxLibSpotify.h
+//  
+//
+//  Created by James Kong on 18/11/13.
+//
+//
+
+#ifndef ____ofxLibSpotify__
+#define ____ofxLibSpotify__
+
+#include <iostream>
+#include "ofMain.h"
+class ofxLibSpotify{
+public:
+    void setup(string user_name, string password);
+    void update();
+private:
+}
+#endif /* defined(____ofxLibSpotify__) */
